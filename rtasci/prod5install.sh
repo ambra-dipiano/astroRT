b84b260effcb980fa78e876c4c1b9e5a13804f47
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-09-12 13:59:11 +0200

    install prod5

--- rtasci/prod5install.sh
+++ rtasci/prod5install.sh
@@ -4,6 +4,7 @@ mkdir tmp
 cd tmp
 wget http://cta.irap.omp.eu/ctools/_downloads/csadd2caldb.py
 wget https://zenodo.org/record/5499840/files/cta-prod5-zenodo-fitsonly-v0.1.zip
-python csadd2caldb.py debug=yes
+unzip cta-prod5-zenodo-fitsonly-v0.1.zip
+csadd2caldb
 cd ..
 rm -rf tmp
