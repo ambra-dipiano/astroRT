e7171accfb424c6b8c91a38602dc5395f86f30d2
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-09-12 13:45:11 +0200

    add unit tests

---
+++ testing/conftest.py
@@ -0,0 +1,15 @@
+# *****************************************************************************
+# Copyright (C) 2023 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *****************************************************************************
+
+import pytest
+import rtadeep
+from os.path import join, dirname, abspath
+
+@pytest.fixture(scope='function')
+def rtadeep_configuration():
+    return join(dirname(abspath(rtadeep.__file__)), 'cfg', 'test.yml')
---
+++ testing/test_rtadeep/test_utils/test_wrap.py
@@ -0,0 +1,15 @@
+# *****************************************************************************
+# Copyright (C) 2023 INAF
+# This software is distributed under the terms of the BSD-3-Clause license
+#
+# Authors:
+# Ambra Di Piano <ambra.dipiano@inaf.it>
+# *****************************************************************************
+
+import pytest
+from rtadeep.utils.wrap import load_yaml_conf
+
+@pytest.mark.rtadeep_configuration
+def test_load_yaml_conf(rtadeep_configuration):
+    configuration = load_yaml_conf(rtadeep_configuration)
+    assert type(configuration) == dict
