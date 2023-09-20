5bbb50a5811dddbe561b3ef262821f2bee866fd0
Ambra Di Piano <ambra.dipiano@inaf.it>
2023-09-20 14:37:52 +0200

    update setup

--- setup.py
+++ setup.py
@@ -8,11 +8,19 @@
 
 from setuptools import setup, find_packages
 
+entry_points = {
+	'console_scripts': [
+		'base_simulator = astrort.simulator.base_simulator:main',
+    ]
+}
+
 setup( 
     name='astrort',
     author='Ambra Di Piano <ambra.dipiano@inaf.it>',
     package_dir={'astrort': 'astrort'},
+    entry_points=entry_points,
     packages=find_packages(),
     include_package_data=True,
     license='BSD-3-Clause',
+    python_requires=">=3.8",
 )
